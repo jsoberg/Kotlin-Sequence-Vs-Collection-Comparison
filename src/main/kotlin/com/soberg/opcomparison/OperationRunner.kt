ae0f57ac67dd57716c642caffe36ea9bad43983e
Joshua Soberg <j15soberg@gmail.com>
2020-12-29 21:31:13 -0500

    Stop using Random, to have more consistent results

--- src/main/kotlin/com/soberg/opcomparison/OperationRunner.kt
+++ src/main/kotlin/com/soberg/opcomparison/OperationRunner.kt
@@ -1,6 +1,5 @@
 package com.soberg.opcomparison
 
-import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
 typealias Operation = (Collection<Int>) -> Unit
@@ -8,8 +7,6 @@ typealias Operation = (Collection<Int>) -> Unit
 class OperationRunner(
     private val collectionSizes: List<Int>
 ) {
-    private val random = Random.Default
-
     /** Creates a [Collection] for each value in [collectionSizes], and calculates the time it takes to run [op].
      * @return a List of [Result]s for running each operation at each size. */
     fun runOperation(op: Operation): List<Result> =
@@ -20,7 +17,7 @@ class OperationRunner(
         }
 
     private fun createListOfSize(size: Int): Collection<Int> =
-        (0 until size).map { random.nextInt() }
+        (0 until size).toList()
 
     data class Result(
         val collectionSize: Int,
